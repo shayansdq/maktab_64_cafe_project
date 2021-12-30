b0abf7a49af5e591ed8d6263678b53fea36972c9
Mobin <Mobinatashi2@gmail.com>
2021-12-30 22:55:56 +0330

    phase 1-1 cashier login fixed some bugs

--- Cashier-Login/app.py
+++ Cashier-Login/app.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ThisSecret'
+app.config['SECRET_KEY'] = 'SECRET_KEY_SECRET_KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://backdoor:123456789@localhost/cafe_login'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
