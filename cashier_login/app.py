e50831e3344d130500b859a9652be533651fe8e0
Mobin <Mobinatashi2@gmail.com>
2021-12-30 22:47:18 +0330

    phase 1-1 cashier login refactor

--- cashier_login/app.py
+++
@@ -1,8 +0,0 @@
-from views import *
-
-
-app.add_url_rule("/", "login_page", login_page, methods=["GET", "POST"])
-app.add_url_rule("/register", "register_page", register_page, methods=["GET", "POST"])
-
-if __name__ == '__main__':
-    app.run()
