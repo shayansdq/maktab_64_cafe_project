b5a198bd321d10ad2db8f8b1a41c82b64dc74a02
mohammadrezaho <mohammadrezasm779@gmail.com>
2022-01-01 02:32:03 +0330

    edit signup script

--- signup.py
+++ signup.py
@@ -1,42 +1,26 @@
 from database import db
+from app import Cashier
 import argparse
 from werkzeug.security import generate_password_hash, check_password_hash
-from abc import ABC
-import psycopg2
-import psycopg2.extras
-from psycopg2._psycopg import connection, cursor
-from app import Cashier
-
-# def create(args):
-#     conn: connection = psycopg2.connect(dbname="flash_cards", user="mohammadreza", host="localhost", port=5432, password=7985)
-#     with conn:
-#         curs = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-#         print('connection created')
-#         with curs:
-#             curs.execute(
-#                 f"""INSERT INTO users(username, password) VALUES (%s, %s);""", (args.username, args.password)
-#                )
-#             return "ok"
-
-
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(100))
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--username", required=True)
-    parser.add_argument("-p", '--password', required=True)
+    parser.add_argument("-f", "--first_name", required=True)
+    parser.add_argument("-l", '--last_name', required=True)
+    parser.add_argument("-u", '--username', required=True)
+    parser.add_argument("-n", '--phone_number', required=True)
+    parser.add_argument("-e", '--email', required=True)
+    parser.add_argument("-p", '--password_hash', required=True)
     args = parser.parse_args()
 
     print('step1')
+    first_name = args.first_name
+    last_name = args.last_name
     username = args.username
-    password = args.password
+    phone_number = args.phone_number
+    email = args.email
+    password_hash = generate_password_hash(args.password_hash, method='sha256')
 
+    user = Cashier(first_name=first_name, last_name=last_name, username=username, phone_number=phone_number, email=email, password_hash=password_hash)
     user.create()
