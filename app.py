51bb4767cf978ec1fe325e0453f1047d79921258
Shayan <shayansadeghi178@gmail.com>
2021-12-31 20:05:01 +0330

    delete some extra attrs and set them true (from login func)

--- app.py
+++ app.py
@@ -1,18 +1,85 @@
-
-from flask import Flask, render_template
-from sqlalchemy.orm import relationship
-
+from flask import Flask, render_template, redirect, url_for
 from database import db
-from os import urandom
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_mobility import Mobility
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from os import urandom
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = urandom(24)
 app.config.from_pyfile('config.cfg')
-
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+##############
+# class User(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(15), unique=True)
+#     email = db.Column(db.String(50), unique=True)
+#     password = db.Column(db.String(128))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Cashier.check_user(int(user_id))
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('E-mail', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    print(form.username.data)
+    if form.validate_on_submit():
+        user = Cashier.check_user(form.username.data)
+        if user:
+            if user.verify_password(form.password.data):
+                return redirect(url_for('dashboard'))
+                # return 'ok'
+        return '<h1>Invalid username or password</h1>'
+    return render_template('login-page.html', form=form)
+
+
+# @app.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     form = RegisterForm()
+#     if form.validate_on_submit():
+#         hashed_password = generate_password_hash(form.password.data, method='sha256')
+#         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+#         db.session.add(new_user)
+#         db.session.commit()
+#         return '<h1>New user has been created!</h1>'
+#     return render_template('register-page.html', form=form)
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', name='shayan20')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 # def create_app():
@@ -23,10 +90,13 @@ db = SQLAlchemy(app)
 #     with app.test_request_context():
 #         db.create_all()
 #     return app
-class Cashier(db.Model):
+
+
+class Cashier(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(32))
     last_name = db.Column(db.String(32))
+    username = db.Column(db.String(15), unique=True)
     phone_number = db.Column(db.String(13), unique=True)
     email = db.Column(db.String(32), unique=True)
     password_hash = db.Column(db.String(128))
@@ -53,7 +123,7 @@ class Cashier(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return '<Member: %r>' % self.username
+        return '<Cashier: %r>' % self.username
 
 
 class Menuitem(db.Model):
@@ -66,7 +136,6 @@ class Menuitem(db.Model):
     is_delete = db.Column(db.Boolean, default=False)
     item_category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     orders = db.relationship('Order', backref='menuitem')
-    # orders = relationship("Order")
 
     def create(self):
         db.session.add(self)
@@ -74,10 +143,10 @@ class Menuitem(db.Model):
 
     @staticmethod
     def find_item(item_name):
-        return MenuItem.query.filter_by(item_name=item_name).first()
+        return Menuitem.query.filter_by(item_name=item_name).first()
 
     def __repr__(self):
-        return '<word: %r>' % self.word
+        return '<menuitem: %r>' % self.word
 
 
 class Table(db.Model):
@@ -104,7 +173,7 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     menu_item_id = db.Column(db.Integer, db.ForeignKey('menuitem.id'))
     table_id = db.Column(db.Integer, db.ForeignKey('table.id'))
-    item_count = db.Column(db.SmallInteger, primary_key=True)
+    item_count = db.Column(db.SmallInteger)
     status = db.Column(db.String(32), default='waiting')
     submit_time = db.Column(db.DateTime)
     is_delete = db.Column(db.Boolean, default=False)
@@ -118,7 +187,7 @@ class Order(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return '<table number: %r>' % self.id
+        return '<order number: %r>' % self.id
 
 
 class Receipt(db.Model):
@@ -140,14 +209,14 @@ class Receipt(db.Model):
         return Receipt.query.filter_by(table_id=table_id).first()
 
     def __repr__(self):
-        return '<word: %r>' % self.word
+        return '<receipt: %r>' % self.word
 
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(32), unique=True)
     duration = db.Column(db.String(32))
-    menu_item = db.relationship('MenuItem', backref='menuitem', lazy='dynamic')
+    menu_item = db.relationship('Menuitem', backref='menuitem', lazy='dynamic')
 
     def create(self):
         db.session.add(self)
@@ -158,7 +227,7 @@ class Category(db.Model):
         return Category.query.filter_by(id=id).first()
 
     def __repr__(self):
-        return '<table number: %r>' % self.id
+        return '<category number: %r>' % self.id
 
 
 @app.route('/')
