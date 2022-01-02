55d90b0c6350347e6a8a3d24ebb1b94b636c60a1
Shayan <shayansadeghi178@gmail.com>
2022-01-02 23:08:38 +0330

    create base_variable for data

--- views.py
+++ views.py
@@ -3,34 +3,49 @@ from database import db
 from flask import request, redirect, url_for, make_response, render_template, flash, make_response
 from forms import *
 
+base_variables = {
+    "page": {
+        "base_title": "Cafe Dark",
+        "lang": 'en-US',
+        "title": 'main'
+    }
+}
+
 
 def login():
+    base_variables['page']['title'] = 'Cashier Login'
+    data = base_variables
     form = LoginForm()
     if request.cookies.get("aetvbhuoaetv"):
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('dashboard',data=data))
     elif form.validate_on_submit():
         user = Cashier.check_user(form.username.data)
         if user:
             if user.verify_password(form.password.data):
-                resp = make_response(redirect(url_for('dashboard')))
+                base_variables['page']['title'] = 'Dashboard'
+                resp = make_response(redirect(url_for('dashboard',data=data)))
                 resp.set_cookie("aetvbhuoaetv", str(user.id))
                 return resp
         return '<h1 style="color:red">Invalid Username/Password</h1>'
-    return render_template('login-page.html', form=form)
+    return render_template('login-page.html', data=data,form=form)
 
 
 def dashboard():
     user_id = request.cookies.get("aetvbhuoaetv")
     if Cashier.get_by_id(user_id):
+        base_variables['page']['title'] = 'Dashboard'
+        data = base_variables
         user_name = Cashier.get_by_id(user_id).username
-        resp = make_response(render_template("dashboard.html", name=str(user_name)))
+        resp = make_response(render_template("dashboard.html", data=data,name=str(user_name)))
         return resp
     else:
-        return redirect(url_for('login'))
+        base_variables['page']['title'] = 'Login'
+        data = base_variables
+        return redirect(url_for('login',data=data))
 
 
 def logout():
-    resp = make_response(redirect(url_for('home')))
+    resp = make_response(redirect(url_for('login')))
     resp.delete_cookie('aetvbhuoaetv')
     return resp
 
@@ -39,10 +54,12 @@ def home():
     from datetime import datetime, timedelta
     now = datetime.now()
     if request.method == "GET":
+        base_variables['page']['title'] = 'Home'
+        data = base_variables
         table_id = request.cookies.get('Table')
-        msg = f'Congrats! your table id is {table_id}'
+        msg = f'Congrats! your table number is: {table_id}'
         tables = Table.query.all()
-        return render_template('index.html', tables=tables, msg=msg)
+        return render_template('index.html', tables=tables, data=data,msg=msg)
     elif request.method == "POST":
         table_id = request.values.get("table_id")
         if table_id:
@@ -53,6 +70,6 @@ def home():
             resp = make_response(redirect(url_for("home")))
             resp.set_cookie("Table", f"{table.id}", expires=now + timedelta(hours=3))
             return resp
-        return "bad request"
+        return redirect(url_for('home'))
     else:
         return "bad request"
