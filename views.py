861ab92206aa55a7c5b22b029dd55e681758859a
Mobin <Mobinatashi2@gmail.com>
2022-01-11 17:56:40 +0330

    finished working on get most popular items

--- views.py
+++ views.py
@@ -102,8 +102,11 @@ def dashboard():
         most_pupolar_items = Order.find_most_popular_items()
         most_pupolar_menu_item = []
         for most_item in most_pupolar_items:
-            most_pupolar_menu_item.append(Menuitem.get_by_id(most_item[0]))
-        print(most_pupolar_menu_item)
+            in_item_id = most_item[0]
+            in_item_count = most_item[1]
+            most_pupolar_menu_item.append(
+                (Menuitem.get_by_id(in_item_id).item_name, Menuitem.get_by_id(in_item_id).price, in_item_count))
+        # print(most_pupolar_menu_item)
         data2 = {
             'menuitem': Menuitem.query.all(),
             'table': Table.query.all(),
@@ -111,7 +114,6 @@ def dashboard():
             'order': Order.query.all(),
             "most_popular": most_pupolar_menu_item
         }
-        # print(data
         resp = make_response(
             render_template("AdminPanel/dashboard.html", data=data,
                             menu=menus, name=str(user_name), data2=data2))
