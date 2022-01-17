6a0652b4f714fea4f15c62bc2c404b548367fc8c
Mobin <Mobinatashi2@gmail.com>
2022-01-17 23:16:19 +0330

    change some things

--- static/AdminPanel/assets/js/settings.js
+++ static/AdminPanel/assets/js/settings.js
@@ -1,19 +1,19 @@
-(function($) {
-  'use strict';
+(function ($) {
+    'use strict';
 
-  $(".ms-settings-toggle").on('click', function(e){
-    $('body').toggleClass('ms-settings-open');
-  });
+    $(".ms-settings-toggle").on('click', function (e) {
+        $('body').toggleClass('ms-settings-open');
+    });
 
-  $("#dark-mode").on('click', function(){
-    $('body').toggleClass('ms-dark-theme');
-  });
-  $("#remove-quickbar").on('click', function(){
-    $('body').toggleClass('ms-has-quickbar');
-  });
-  $("#hide-aside-left").on('click', function(){
-    $('body').toggleClass('ms-aside-left-open');
-  });
+    $("#dark-mode").on('click', function () {
+        $('body').toggleClass('ms-dark-theme');
+    });
+    $("#remove-quickbar").on('click', function () {
+        $('body').toggleClass('ms-has-quickbar');
+    });
+    $("#hide-aside-left").on('click', function () {
+        $('body').toggleClass('ms-aside-left-open');
+    });
 
 
 })(jQuery);
